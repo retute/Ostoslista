34931b792093487a44334ce20a179744264df35a
Reetta <reetta.tervo@helsinki.fi>
2019-03-24 17:02:30 +0200

    viikon 3 osiota materiaalista: kirjautuminen

--- application/__init__.py
+++ application/__init__.py
@@ -7,9 +7,30 @@ app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
+#toiminnallisuus
 from application import views
 
 from application.items import models
 from application.items import views
+
+from application.auth import models
+from application.auth import views
+
+#kirjautuminen
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
   
+  #tietokantataulut
 db.create_all()
---
+++ application/auth/views.py
@@ -0,0 +1,23 @@
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user
+
+from application import app
+from application.auth.models import User
+from application.auth.forms import LoginForm
+
+@app.route("/auth/login", methods = ["GET", "POST"])
+def auth_login():
+    if request.method == "GET":
+        return render_template("auth/loginform.html", form = LoginForm())
+
+    form = LoginForm(request.form)
+    # lis‰‰ t‰h‰n ehk‰
+
+    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    if not user:
+        return render_template("auth/loginform.html", form = form,
+                               error = "Username or password doesn't match. Try again!")
+
+
+    login_user(user)
+    return redirect(url_for("index"))    
