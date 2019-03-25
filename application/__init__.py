2c98123a634927fd8629201150c1ce2c1b117cc3
Reetta <reetta.tervo@helsinki.fi>
2019-03-25 15:30:54 +0200

    herokun lisäjuttuja

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE:URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
