2cbffa6c4ac1415a42145b7b629f5340e89135d4
Reetta <reetta.tervo@helsinki.fi>
2019-05-03 11:11:41 +0300

    turha models.py poistettu

--- application/models.py
+++
@@ -1,8 +0,0 @@
-from application import db
-
-class Base(db.Model):
-
-    __abstract__ = True
-  
-#    id = db.Column(db.Integer, primary_key=True)
-#    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
